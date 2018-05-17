cc60e24b522b427b4ec95ea2760cefcc2ff7cc82
Jed Peek <jedpeek@yahoo.com>
2018-05-17 14:49:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Dog
+def bark
+  puts "Woof!"
+end
+
+def name(dog_name)
+  @name = dog_name
+end
+
+def name=(dog_name)
+  @name = dog_name
+end
+
+def name
+  name = @name
+end
